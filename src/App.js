790f5e88ad81f70cc7376de2eac54dccf70b583d
ColinH58 <choolwerf@gmail.com>
2022-07-28 13:48:32 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { GroceryList, Form } from "./Components";
 
-const LOCAL_STORAGE_KEY = "react-grocery-list";
-
 function App() {
   const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    const storeItems = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (storeItems) {
-      setItems(storeItems);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(items));
-  }, [items]);
 
   function addItems(item) {
     setItems([item, ...items]);
